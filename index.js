86ea8e22e1045527c48149cd796b7853732fe1e5
HarryMosdell <up712364@myport.ac.uk>
2024-09-27 21:22:02 +0100

    Update index.js
    ALTERING JS SLIGHTLY

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-  const hamburger = document.getElementById('hamburger_menu');
+    <script> 
+
+        const hamburger = document.getElementById('hamburger_menu');
         const navbar = document.getElementById('mobile_nav');
-    
+        const crossButton = document.getElementById('cross');
+
         hamburger.addEventListener('click', () => {
-            navbar.classList.toggle('left'); // Toggles the class on click
+            navbar.classList.toggle('left');
+            navbar.classList.toggle('d-none');
+            
         });
         
-         const crossButton = document.getElementById('cross');
-    
          crossButton.addEventListener('click', () => {
             navbar.classList.toggle('left'); // Toggles the class on click
+            
+        } );
+
+
+            navbar.addEventListener('transitionend', () => {
+            navbar.classList.toggle('d-none');
+            
         });
+    </script>
